1b6b3fd684bda0c0dd7501c473d235b058b2b16f
MauryaAbhi378 <abhim0505@gmail.com>
2025-10-03 00:40:45 +0530

    Update : Connect the MongoDB to project

---
+++ backend/user/src/config/connectDB.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    const url = process.env.MONGO_URI
+
+    if (!url) {
+        throw new Error("MONGO_URI is not defined in environment variable")
+    }
+
+    try {
+        await mongoose.connect(url, {
+            dbName: "chatapp"
+        })
+        console.log("Connected to MongoDB")
+    } catch (err) {
+        console.log("Failed to connect to MongoDB", err)
+        process.exit(1) //Stops the Node.js application immediately with an error code 1.
+    }
+}
+
+export default connectDB
