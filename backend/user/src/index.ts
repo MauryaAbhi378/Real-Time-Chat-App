765b495e09660f9590fa670a824b6003057f9cf6
MauryaAbhi378 <abhim0505@gmail.com>
2025-10-03 16:35:15 +0530

    Update : Establish Connection to Upstash

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,14 +1,29 @@
-import express from "express"
-import dotenv from"dotenv"
-import connectDB from "./config/connectDB.js"
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/connectDB.js";
+import { Redis } from "@upstash/redis";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+export const redisClient = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
-const PORT = process.env.PORT 
+(async () => {
+  try {
+    const pong = await redisClient.ping();
+    console.log("Connected to Redis:", pong);
+  } catch (err) {
+    console.error("Redis connection error:", err);
+  }
+})();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
-})
+  console.log(`Server running on ${PORT}`);
+});
